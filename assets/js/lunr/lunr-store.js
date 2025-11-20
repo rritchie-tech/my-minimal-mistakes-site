0831ff3e54abe910ce8c79508dfcffc27186bc29
rritchie-tech <rritchie-tech@users.noreply.github.com>
2025-11-20 04:27:20 +0000

    deploy: 46fb2b975406af350801f1f62440ff03b30d4c2d

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Sample blog post to learn markdown tips",
         "excerpt":"This is a demo post to show you how to write blog posts with markdown. I strongly encourage you to take 5 minutes to learn how to write in markdown - it’ll teach you how to transform regular text into bold/italics/tables/etc. I also encourage you to look at the code...","categories": [],
         "tags": ["test"],
-        "url": "/sample-markdown/",
+        "url": "/my-minimal-mistakes-site/sample-markdown/",
         "teaser": null
       }]
